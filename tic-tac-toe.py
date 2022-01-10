4d8031f485c13712b0492a6dca0408c9cab4d31f
Eric Costa <eric_costa10@hotmail.com>
2022-01-10 20:16:16 -0300

    add note with assigment and author name

---
+++ tic-tac-toe.py
@@ -0,0 +1,7 @@
+"""This is the Tic-Tac-Toe game in which two players seek
+in alternate turns to complete a row, a column, or a 
+diagonal with either three x's or three o's drawn in 
+the spaces of a grid of nine squares.
+Author: Eric Costa"""
+
+
